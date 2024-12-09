e559e7caeda60ecd4850f4e7c9892b62d97eca96
Akhmad Ali Laviani <51687742+akhmadali22@users.noreply.github.com>
2024-12-09 10:56:27 +0700

    feat: Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Function to handle the "Contact Me" button click
+document.getElementById('contactButton').addEventListener('click', function() {
+  alert('You can contact me at akhmadali22@gmail.com');
+});
